b61e5a071772bd748166f4625ef7be7c01ed8037
btech240 <brenttlong@gmail.com>
2024-02-02 10:01:49 -0500

    Added logic to count known/unknown words and print the count after every selection

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ df = pd.read_csv("data/french_words.csv")
 # Convert the dataframe to python dictionary
 to_learn = df.to_dict("records")
 current_card = {}
+known_unknown = {"known": 0, "unknown": 0}
 
 
 def next_card():
@@ -29,6 +30,20 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    global known_unknown
+    known_unknown["known"] += 1
+    print(known_unknown)
+    next_card()
+
+
+def is_unknown():
+    global known_unknown
+    known_unknown["unknown"] += 1
+    print(known_unknown)
+    next_card()
 
 
 # Initialize the window for the application
@@ -53,13 +68,13 @@ canvas.grid(column=0, row=0, columnspan=2)
 # Create "know" button
 cross_image = PhotoImage(file="images/wrong.png")
 unknown_button = Button(
-    image=cross_image, highlightthickness=0, command=next_card)
+    image=cross_image, highlightthickness=0, command=is_unknown)
 unknown_button.grid(column=0, row=1)
 
 # Create "dont know" button
 check_image = PhotoImage(file="images/right.png")
 known_button = Button(
-    image=check_image, highlightthickness=0, command=next_card)
+    image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(column=1, row=1)
 
 next_card()
